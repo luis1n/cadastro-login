b158d7cd6df0ed7d97ad50d20f93f106e70927ca
luisalberto <luisalberto@sefaz.pi.gov.br>
2023-04-03 10:01:00 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-app-bar>
+  <div id="app">
+    <nav class="navbar navbar-expand-lg navbar-company">
+      <a class="navbar-brand" href="#">Company</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/gerentes" class="nav-link">
+              Gerentes
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/login" class="nav-link">
+              Entrar
+            </router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <router-view/>
+  </div>
+</v-app-bar>
+</template>
+
+<style>
+.navbar {
+  background: #00008b;
+}
+.navbar-company a {
+  color: #fff;
+}
+.navbar-company a:hover {
+  color: #000;
+}
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="container">
+        <h1>Login</h1>
+        <form>
+            <div class="form-group">
+                <label for="email">E-mail</label>
+                <input type="email" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="senha">Senha</label>
+                <input type="password" class="form-control">
+            </div>
+            <v-btn type="submit" class="btn btn-primary brn-block">Entrar</v-btn>
+            <router-link :to="{name: 'novo.usuario' }">
+                Não possui um cadastro? Cadastre-se aqui!
+            </router-link>
+        </form>
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    data() {
+        return {
+            usuario: {}
+        }
+    }
+}
+</script>
